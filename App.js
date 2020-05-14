0d392fbda46b2a84d0e267169b8d3d900dfa14d3
selikhovamary <maryiaselikhova@gmail.com>
2020-05-14 19:30:00 +0300

    packages

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+ import useCachedResources from './hooks/useCachedResources';
+ import BottomTabNavigator from './navigation/BottomTabNavigator';
+ import LinkingConfiguration from './navigation/LinkingConfiguration';
+import { AppLoading } from 'expo';
+import { Container, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
@@ -27,7 +31,35 @@ export default function App(props) {
     );
   }
 }
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isReady: false,
+//     };
+//   }
 
+//   async componentDidMount() {
+//     await Font.loadAsync({
+//       Roboto: require('native-base/Fonts/Roboto.ttf'),
+//       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+//       ...Ionicons.font,
+//     });
+//     this.setState({ isReady: true });
+//   }
+
+//   render() {
+//     if (!this.state.isReady) {
+//       return <AppLoading />;
+//     }
+
+//     return (
+//       <Container>
+//         <Text>Open up App.js to start working on your app!</Text>
+//       </Container>
+//     );
+//   }
+// }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
